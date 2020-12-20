b0606f85ce1f9fedcf1fc022840999faea239b1e
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-12-20 22:44:43 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! A small PID controller library
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, future_incompatible, unreachable_pub)]
